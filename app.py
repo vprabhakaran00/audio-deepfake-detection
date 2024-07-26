2aee8a353ecf4d941621dcb86fa3d3ce46645c13
vprabhakaran00 <vishnuprbhkrn@gmail.com>
2024-07-25 21:05:41 -0700

    Changed heading in prediction app

--- app.py
+++ app.py
@@ -120,4 +120,4 @@ def index():
     return render_template('index.html', prediction = '', confidence = '')
 
 if __name__ == '__main__':
-    app.run(port = 8080, debug=True)
+    app.run(port = 8080)
